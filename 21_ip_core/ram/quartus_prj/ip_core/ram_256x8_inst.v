bedf30bafacfe6d1234a0f1f7c511f6688d7e8f1
胡远涛 <810827208@qq.com>
2020-07-14 11:48:25 +0800

    调整工程顺序

--- 21_ip_core/ram/quartus_prj/ip_core/ram_256x8_inst.v
+++
@@ -1,9 +0,0 @@
-ram_256x8	ram_256x8_inst (
-	.aclr ( aclr_sig ),
-	.address ( address_sig ),
-	.clock ( clock_sig ),
-	.data ( data_sig ),
-	.rden ( rden_sig ),
-	.wren ( wren_sig ),
-	.q ( q_sig )
-	);
--- 22_freq_meter/quartus_prj/ip_core/clk_gen/clk_gen_inst.v
+++
@@ -1,5 +0,0 @@
-clk_gen	clk_gen_inst (
-	.areset ( areset_sig ),
-	.inclk0 ( inclk0_sig ),
-	.c0 ( c0_sig )
-	);
