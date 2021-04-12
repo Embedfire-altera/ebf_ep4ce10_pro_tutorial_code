92b22b0af4280afcda68852fbbcb57c26dd3f42e
huyt <810827208@qq.com>
2021-04-12 17:36:10 +0800

    修复VGA彩条显示工程vga控制模块仿真问题

--- 19_vga_colorba/sim/tb_vga_ctrl.v
+++ 19_vga_colorba/sim/tb_vga_ctrl.v
@@ -22,9 +22,9 @@ module  tb_vga_ctrl();
 //****************** Parameter and Internal Signal *******************//
 //********************************************************************//
 //wire  define
-wire            locked      ;
-wire            rst_n       ;
-wire            vga_clk     ;
+//wire            locked      ;
+//wire            rst_n       ;
+//wire            vga_clk     ;
 
 //reg   define
 reg             sys_clk     ;
@@ -45,14 +45,14 @@ initial
     end
 
 //sys_clk：产生时钟
-always  #10 sys_clk = ~sys_clk;
+always  #20 sys_clk = ~sys_clk;
 
 //rst_n:VGA模块复位信号
-assign  rst_n = (sys_rst_n & locked);
+//assign  rst_n = (sys_rst_n & locked);
 
 //pix_data:输入像素点色彩信息
-always@(posedge vga_clk or negedge rst_n)
-    if(rst_n == 1'b0)
+always@(posedge sys_clk or negedge sys_rst_n)
+    if(sys_rst_n == 1'b0)
         pix_data    <=  16'h0;
     else
         pix_data    <=  16'hffff;
@@ -62,19 +62,19 @@ always@(posedge vga_clk or negedge rst_n)
 //********************************************************************//
 
 //------------- clk_gen_inst -------------
-clk_gen clk_gen_inst
+/* clk_gen clk_gen_inst
 (
     .areset     (~sys_rst_n ),  //输入复位信号,高电平有效,1bit
     .inclk0     (sys_clk    ),  //输入50MHz晶振时钟,1bit
     .c0         (vga_clk    ),  //输出VGA工作时钟,频率25Mhz,1bit
     .locked     (locked     )   //输出pll locked信号,1bit
-);
+); */
 
 //------------- vga_ctrl_inst -------------
 vga_ctrl  vga_ctrl_inst
 (
-    .vga_clk    (vga_clk    ),  //输入工作时钟,频率25MHz,1bit
-    .sys_rst_n  (rst_n      ),  //输入复位信号,低电平有效,1bit
+    .vga_clk    (sys_clk    ),  //输入工作时钟,频率25MHz,1bit
+    .sys_rst_n  (sys_rst_n  ),  //输入复位信号,低电平有效,1bit
     .pix_data   (pix_data   ),  //输入像素点色彩信息,16bit
 
     .pix_x      (pix_x      ),  //输出VGA有效显示区域像素点X轴坐标,10bit
